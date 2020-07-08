3324b4f03e283ef84d218e311c251005658fde71
sampresh <slevinshrestha@gmail.com>
2020-07-08 21:44:29 +1000

    Announcement section WIP

--- src/components/Announcements.vue
+++ src/components/Announcements.vue
@@ -4,8 +4,23 @@
     </div>
 </template>
 <script>
+import axios from 'axios'
 	export default{
-        name:'Announcements'			
+        name:'Announcements',
+        data() {
+            return{
+            }
+        },	
+        mounted(){
+            axios
+            .get('https://covidtracking.com/api/states')
+            .then((response) => { response.data.forEach(eachState => {
+                                    this.states.push(eachState.state) 
+                                    this.positives.push(eachState.positive) 
+                                })
+            })
+
+        }		
 		
 	}
 </script>
